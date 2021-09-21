c2d8b89085ec388042a81c56ec739a54e378d350
Breadmaker91 <europe_elmen@hotmail.com>
2021-09-21 20:00:30 +0200

    Bugfix & Cosmetic Update
    - Fixed bug which made the mod unusable in 2.7.6
    - Added new liveries
    - Added new smokepods

--- DCS-SK60-Mod/Liveries/SK-60/TRAINER #63/description.lua
+++ DCS-SK60-Mod/Liveries/SK-60/TRAINER #63/description.lua
@@ -1,12 +1,11 @@
 livery = {
-	{"SK60_body1",		DIFFUSE				,	"SK60_body1", false};
-	{"SK60_body1",		NORMAL_MAP			,	"SK60_body1_normal", false};
-	{"SK60_body1",		ROUGHNESS_METALLIC	,	"SK60_body1_roughmet", false};
-	{"SK60_body2",		DIFFUSE				,	"SK60_body2", false};
-	{"SK60_body2",		NORMAL_MAP			,	"sk60_body2_normal", false};
-	{"SK60_body2",		ROUGHNESS_METALLIC	,	"SK60_body2_roughmet", false};
-	{"HELMET BAKED",	DIFFUSE				,	"SK60_helmet", false};
-	{"HELMET BAKED",	NORMAL_MAP			,	"SK60_helmet_normal", false};
-	{"HELMET BAKED",	ROUGHNESS_METALLIC	,	"SK60_helmet_roughmet", false};
+	{"SK60_body1",				DIFFUSE				,	"SK60_body1", 				false};
+	{"SK60_body1",				ROUGHNESS_METALLIC	,	"SK60_body1_roughmet", 		false};
+	{"SK60_body2",				DIFFUSE				,	"SK60_body2", 				false};
+	{"SK60_body2",				ROUGHNESS_METALLIC	,	"SK60_body2_roughmet", 		false};
+	{"SK60_gear_doors",			DIFFUSE				,	"SK60_gear_doors", 			false};
+	{"SK60_gear_doors",				ROUGHNESS_METALLIC	,	"SK60_gear_doors_roughmet", 		false};
+	{"SK60_GearAndAirbrake",	DIFFUSE				,	"SK60_GearAndAirbrake", 	false};
+	{"SK60_GearAndAirbrake",	ROUGHNESS_METALLIC	,	"SK60_GearAndAirbrake_roughmet", 		false};
 }
 name = "TRAINER #63'"	
