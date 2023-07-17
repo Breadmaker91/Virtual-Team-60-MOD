df596cfb5b3bfb5c24465d55b29e4566df36731b
Breadmaker91 <europe_elmen@hotmail.com>
2023-07-17 16:40:40 +0200

    Model and Livery Update

---
+++ DCS-SK60-Mod/Liveries/SK-60/NEW TEAM 60/description.lua
@@ -0,0 +1,32 @@
+livery = {
+	{"SK60_body1",		DIFFUSE				,	"SK60_body1", false};
+	{"SK60_body1",		ROUGHNESS_METALLIC	,	"SK60_body1_roughmet", false};
+	{"SK60_body1",		NORMAL				,	"SK60_body1_normal", false};
+	{"SK60_body2",		DIFFUSE				,	"SK60_body2", false};
+	{"SK60_body2",		ROUGHNESS_METALLIC	,	"SK60_body2_roughmet", false};
+	{"SK60_body2",		NORMAL				,	"SK60_body2_normal", false};
+	{"SK60_Body3.003",		DIFFUSE				,	"SK60_body3", false};
+	{"SK60_Body3.003",		ROUGHNESS_METALLIC	,	"SK60_body3_roughmet", false};
+	{"SK60_Body3.003",		NORMAL				,	"SK60_body3_normal", false};	
+	{"SK60_gear_doors",			DIFFUSE				,	"SK60_gear_doors", 			false};
+	{"SK60_gear_doors",				ROUGHNESS_METALLIC	,	"SK60_gear_doors_roughmet", 		false};	
+	{"SK60_GearAndAirbrake",	DIFFUSE				,	"SK60_GearAndAirbrake", 	false};
+	{"SK60_GearAndAirbrake",	ROUGHNESS_METALLIC	,	"SK60_GearAndAirbrake_roughmet", 		false};
+	{"SK60_TailFins",			DIFFUSE				,	"SK60_TailFin", 						false};
+	{"SK60_TailFins",			ROUGHNESS_METALLIC	,	"SK60_TailFin_roughmet", 				false};
+	{"SK60_TailFins",			NORMAL				,	"SK60_TailFin_normal", 				false};
+	{"SK60_Helmet",				DIFFUSE				,	"SK60_helmet", false};
+	{"SK60_Helmet",				ROUGHNESS_METALLIC	,	"SK60_helmet_roughmet", false};
+	{"SK60_Helmet",				NORMAL				,	"SK60_helmet_normal", false};
+	{"SK60_Mask",				DIFFUSE				,	"SK60_mask", false};
+	{"SK60_Mask",				ROUGHNESS_METALLIC	,	"SK60_mask_roughmet", false};
+	{"SK60_Mask",				NORMAL				,	"SK60_mask_normal", false};
+	{"Pilot_Body",	DIFFUSE				,	"Pilot_Suit", false};
+	{"Pilot_Body",	ROUGHNESS_METALLIC	,	"Pilot_Suit_roughmet", false};
+	{"Pilot_Body",	NORMAL				,	"Pilot_Suit_normal", false};
+	{"SK60_pilothead",	DIFFUSE				,	"SK60_pilothead", false};
+	{"Kneeboard_Page",	DIFFUSE				,	"SK60_KneeboardPage", false};
+	
+
+}
+name = "NEW TEAM60 #1"	
